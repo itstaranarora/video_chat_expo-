1cf0d2f1bb83d906c1a462d5276fe51e5b2889f6
Taran Arora <itstaranamail.com>
2022-01-06 21:49:40 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import React, { useState } from "react";
+import { Button } from "react-native";
+import AgoraUIKit from "agora-rn-uikit";
+const App = () => {
+  const [videoCall, setVideoCall] = useState(true);
+  const rtcProps = {
+    appId: "49762b19264945e99da818909eb02cfd",
+    channel: "testtest",
+    token:
+      "00649762b19264945e99da818909eb02cfdIACav9MC4mszgKttvf9sQ5VD3kNjo5iM/0tYW1i/ixWH9TG7aewAAAAAEACIFpSN8y7YYQEAAQDzLthh",
+  };
+  const callbacks = { EndCall: () => setVideoCall(false) };
+  return videoCall ? (
+    <AgoraUIKit
+      rtcProps={{
+        appId: "API_ID",
+        channel: "test",
+      }}
+      callbacks={callbacks}
+    />
+  ) : (
+    <Button title="Join" onPress={() => setVideoCall(true)} />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
